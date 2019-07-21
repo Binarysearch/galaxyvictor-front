d21a93e135e265ac768ce2f3f14f2d989eee6176
Binarysearch <desenlace.net@gmail.com>
2019-07-21 10:54:00 +0200

    Añadidos iconos

--- galaxyvictor/src/app/app.component.ts
+++ galaxyvictor/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 export interface AppRoute {
   path: string;
   title: string;
+  faIcon?: string;
 }
 
 @Component({
@@ -15,16 +16,16 @@ export class AppComponent {
   routes: AppRoute[] = [
     { path: '/develop', title: 'Develop' },
     { path: '/admin', title: 'Admin' },
-    { path: '/universe', title: 'Universe' },
-    { path: '/galaxy', title: 'Galaxy' },
-    { path: '/civilizations', title: 'Civilizations' },
-    { path: '/colonies', title: 'Colonies' },
-    { path: '/fleets', title: 'Fleets' },
-    { path: '/planets', title: 'Planets' },
-    { path: '/trade', title: 'Trade' },
-    { path: '/research', title: 'Research' },
-    { path: '/battles', title: 'Battles' },
-    { path: '/', title: 'Home' }
+    { path: '/universe', title: 'Universe', faIcon: 'fab fa-galactic-republic' },
+    { path: '/galaxy', title: 'Galaxy', faIcon: 'fa fa-atom' },
+    { path: '/civilizations', title: 'Civilizations', faIcon: 'fab fa-galactic-senate' },
+    { path: '/colonies', title: 'Colonies', faIcon: 'fas fa-globe' },
+    { path: '/fleets', title: 'Fleets', faIcon: 'fas fa-rocket' },
+    { path: '/planets', title: 'Planets', faIcon: 'fas fa-globe-europe' },
+    { path: '/trade', title: 'Trade', faIcon: 'fas fa-handshake' },
+    { path: '/research', title: 'Research', faIcon: 'fas fa-flask' },
+    { path: '/battles', title: 'Battles', faIcon: 'fas fa-fighter-jet'  },
+    { path: '/', title: 'Home', faIcon: 'fas fa-home' }
   ];
 
   constructor(){}
