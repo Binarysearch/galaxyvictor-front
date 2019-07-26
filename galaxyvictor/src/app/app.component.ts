c4c7b05ee03070b41b87a8184b9b5516c55873d3
Binarysearch <desenlace.net@gmail.com>
2019-07-26 19:37:34 +0200

    Actualizado dashboard

--- galaxyvictor/src/app/app.component.ts
+++ galaxyvictor/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DsConfig, TopbarPosition } from '@binarysearch/dashboard';
 import { SocketService } from './services/socket.service';
 
 export interface AppRoute {
@@ -14,22 +15,24 @@ export interface AppRoute {
 })
 export class AppComponent implements OnInit{
   
-  routes: AppRoute[] = [
-    { path: '/', title: 'Home', faIcon: 'fas fa-home' },
-    { path: '/develop', title: 'Develop', faIcon: 'fas fa-file-code' },
-    { path: '/admin', title: 'Admin', faIcon: 'fas fa-tools' },
-    { path: '/universe', title: 'Universe', faIcon: 'fab fa-galactic-republic' },
-    { path: '/galaxy', title: 'Galaxy', faIcon: 'fa fa-atom' },
-    { path: '/civilizations', title: 'Civilizations', faIcon: 'fab fa-galactic-senate' },
-    { path: '/colonies', title: 'Colonies', faIcon: 'fas fa-globe' },
-    { path: '/fleets', title: 'Fleets', faIcon: 'fas fa-rocket' },
-    { path: '/planets', title: 'Planets', faIcon: 'fas fa-globe-europe' },
-    { path: '/trade', title: 'Trade', faIcon: 'fas fa-handshake' },
-    { path: '/research', title: 'Research', faIcon: 'fas fa-flask' },
-    { path: '/battles', title: 'Battles', faIcon: 'fas fa-fighter-jet'  },
-    { path: '/login', title: 'Login', faIcon: 'fas fa-sign-in-alt' },
-    { path: '/register', title: 'Register', faIcon: 'fas fa-user-plus' },
-  ];
+  config: DsConfig = {
+    routes: [
+      { path: '/', title: 'Home', faIcon: 'fas fa-home' },
+      { path: '/develop', title: 'Develop', faIcon: 'fas fa-file-code' },
+      { path: '/admin', title: 'Admin', faIcon: 'fas fa-tools' },
+      { path: '/universe', title: 'Universe', faIcon: 'fab fa-galactic-republic' },
+      { path: '/galaxy', title: 'Galaxy', faIcon: 'fa fa-atom' },
+      { path: '/civilizations', title: 'Civilizations', faIcon: 'fab fa-galactic-senate' },
+      { path: '/colonies', title: 'Colonies', faIcon: 'fas fa-globe' },
+      { path: '/fleets', title: 'Fleets', faIcon: 'fas fa-rocket' },
+      { path: '/planets', title: 'Planets', faIcon: 'fas fa-globe-europe' },
+      { path: '/trade', title: 'Trade', faIcon: 'fas fa-handshake' },
+      { path: '/research', title: 'Research', faIcon: 'fas fa-flask' },
+      { path: '/battles', title: 'Battles', faIcon: 'fas fa-fighter-jet'  },
+      { path: '/login', title: 'Login', faIcon: 'fas fa-sign-in-alt', topbarPosition: TopbarPosition.RIGHT },
+      { path: '/register', title: 'Register', faIcon: 'fas fa-user-plus', topbarPosition: TopbarPosition.RIGHT },
+    ]
+  }
 
   constructor(private socket: SocketService){}
 
