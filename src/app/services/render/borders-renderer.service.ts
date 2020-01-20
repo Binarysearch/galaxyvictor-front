fee3b1bee96c1710f0593a54690b82116ca18d08
Binarysearch <desenlace.net@gmail.com>
2020-01-20 21:27:43 +0100

    a

--- src/app/services/render/borders-renderer.service.ts
+++ src/app/services/render/borders-renderer.service.ts
@@ -5,6 +5,7 @@ import { BORDERS_VS_SOURCE, BORDERS_FS_SOURCE } from './shaders/borders-shader';
 import { ColorService } from '../color.service';
 import { BorderRect } from '../borders.service';
 import { Store } from '../data/store';
+import { Colony } from 'src/app/model/colony';
 
 @Injectable({
   providedIn: 'root'
@@ -15,10 +16,11 @@ export class BordersRendererService {
   aspectUniformLocation: WebGLUniformLocation;
   zoomUniformLocation: WebGLUniformLocation;
   positionUniformLocation: WebGLUniformLocation;
-  borderChunks: BordersChunk[] = [];
-  backgroundBorderChunks: BordersChunk[] = [];
+  vao: WebGLVertexArrayObjectOES;
 
   private worker: Worker;
+  texture: WebGLTexture;
+  colonies: Set<Colony>;
 
   constructor(
     private shaderCompiler: ShaderCompilerService,
@@ -28,6 +30,7 @@ export class BordersRendererService {
 
   setup(context: RenderContext): void {
     const gl = context.gl;
+    const gl2 = <WebGL2RenderingContext>context.gl;
     this.program = this.shaderCompiler.createShaderProgram(gl, BORDERS_VS_SOURCE, BORDERS_FS_SOURCE);
 
     this.aspectUniformLocation = gl.getUniformLocation(this.program, 'aspect');
@@ -36,35 +39,58 @@ export class BordersRendererService {
 
 
     this.store.getColonies().subscribe(colonies => {
+      
 
       const civilizationColors: Map<string, {r: number; g: number; b: number; }> = new Map();
       colonies.forEach(c => civilizationColors.set(c.civilization.id, this.colorService.getCivilizationColor(c.civilization.id)));
 
-      if (this.worker) {
-        this.worker.terminate();
+      const c: Colony = colonies.values().next().value;
+      if(c){
+        this.colonies = colonies;
+
+        const f = 256*256*256*255;
+        const cx = 60000;
+        const x = f*(1 + cx / 60000) / 2;
+        const x1 = x / (256.0 * 256.0 * 256.0);
+        const x2 = x / (256.0 * 256.0) % 256;
+        const x3 = x / (256.0) % 256;
+        const x4 = x % 256;
+
+        const arr = new Uint8Array([x1, x2, x3, x4]);
+        console.log([x1, x2, x3, x4]);
+        console.log(arr);
+
+        this.texture = gl.createTexture();
+        gl.bindTexture(gl.TEXTURE_2D, this.texture);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl2.RGBA, 1, 1, 0, gl2.RGBA, gl2.UNSIGNED_BYTE, arr);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
       }
-
-      this.worker = new Worker('../../workers/border-generator.worker', { type: 'module' });
-      
-      this.worker.onmessage = ({ data }) => {
-        if (data === 'START') {
-          this.backgroundBorderChunks = [];
-        } else if (data === 'END') {
-          this.borderChunks = this.backgroundBorderChunks;
-        } else if (data.triangleCount > 0) {
-          this.backgroundBorderChunks.push(new BordersChunk(gl, this.program, data.data, data.triangleCount));
-        }
-      };
-
-      this.worker.postMessage({ colonies: colonies, civilizationColors: civilizationColors });
 
     });
 
+    this.vao = (gl as any).createVertexArray();
+    (gl as any).bindVertexArray(this.vao);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([-60000, 60000,  -60000, -60000,  60000, -60000,  60000, 60000,  -60000, 60000,  60000, -60000]), gl.STATIC_DRAW);
+
+    const coord = gl.getAttribLocation(this.program, 'position');
+    gl.vertexAttribPointer(coord, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(coord);
+
+
+    ///////////////////////////////
     
-    //this.borderChunks.push(new BordersChunk(gl, this.program, new Float32Array([-1, 0, 1,0,0,0.2,  -1, -1, 1,0,0,0.2,  0, -1, 1,0,0,0.2,  0, 0, 1,0,0,0.2,  -1, 0, 1,0,0,0.2,  0, -1, 1,0,0,0.2]), 6));
+    
+    ////////////////////////////
+    
   }
 
   render(context: RenderContext): void {
+    if(!this.colonies || this.colonies.size === 0){
+      return;
+    }
     const camera = context.camera;
     const aspect = context.aspectRatio;
     const gl = context.gl;
@@ -79,104 +105,16 @@ export class BordersRendererService {
     gl.uniform1f(this.aspectUniformLocation, aspect);
 
     gl.uniform2f(this.positionUniformLocation, -camera.x, -camera.y);
+    gl.uniform2f(this.positionUniformLocation, -60000, 0);
 
-    this.borderChunks.forEach(bc => {
-      gl2.bindVertexArray(bc.vao);
-      gl.drawArrays(gl.TRIANGLES, 0, bc.triangleCount*3);
-    });
+    //////////////////
+    gl.bindTexture(gl.TEXTURE_2D, this.texture);
+
+    ///////////////////////////////////
+
+    gl2.bindVertexArray(this.vao);
+    gl.drawArrays(gl.TRIANGLES, 0, 6);
     
 
   }
-}
-
-class BordersChunk {
-  vao: WebGLVertexArrayObjectOES;
-
-  constructor(gl: WebGLRenderingContext, program: WebGLShader, vertices: Float32Array, public triangleCount: number) {
-    this.vao = (gl as any).createVertexArray();
-    (gl as any).bindVertexArray(this.vao);
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
-    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-
-    const coord = gl.getAttribLocation(program, 'position');
-    gl.vertexAttribPointer(coord, 2, gl.FLOAT, false, 6*4, 0);
-    gl.enableVertexAttribArray(coord);
-
-    const color = gl.getAttribLocation(program, 'v_color');
-    gl.vertexAttribPointer(color, 4, gl.FLOAT, false, 6*4, 2*4);
-    gl.enableVertexAttribArray(color);
-  }
-}
-
-class BordersTree {
-
-  tl: BordersTree;
-  tr: BordersTree;
-  bl: BordersTree;
-  br: BordersTree;
-
-  x1: number;
-  x2: number;
-  y1: number;
-  y2: number;
-
-  render(context: RenderContext, vp: Rect) {
-    if (this.enoughtResolution(vp)) {
-      this._render(context);
-      return;
-    }
-    
-    const mx = (this.x1 + this.x2) / 2;
-    const my = (this.y1 + this.y2) / 2;
-    
-    const tl = vp.x1 < mx && vp.y1 < my;
-    const tr = vp.x2 > mx && vp.y1 < my;
-    const bl = vp.x1 < mx && vp.y2 > my;
-    const br = vp.x2 > mx && vp.y2 > my;
-
-    let childrenCanRender = true;
-
-    if (tl && !this.tl) {
-      //crear hijo tl
-      childrenCanRender = false;
-    }
-    if (tr && !this.tr) {
-      //crear hijo tr
-      childrenCanRender = false;
-    }
-    if (bl && !this.bl) {
-      //crear hijo tbll
-      childrenCanRender = false;
-    }
-    if (br && !this.br) {
-      //crear hijo br
-      childrenCanRender = false;
-    }
-
-    if (!childrenCanRender) {
-      this._render(context);
-    } else {
-      tl || this.tl.render(context, vp);
-      tr || this.tr.render(context, vp);
-      bl || this.bl.render(context, vp);
-      br || this.br.render(context, vp);
-    }
-  }
-
-  private enoughtResolution(vp: Rect) {
-    //La pantalla abarca mas de la mitad
-    return vp.x2 - vp.x1 > (this.x2 - this.x1) / 2;
-  }
-
-  private _render(context: RenderContext) {
-
-  }
-}
-
-interface Rect {
-  x1: number;
-  x2: number;
-  y1: number;
-  y2: number;
 }
--- src/app/services/render/shaders/borders-shader.ts
+++ src/app/services/render/shaders/borders-shader.ts
@@ -1,30 +1,48 @@
 export const BORDERS_VS_SOURCE = `#version 300 es
 
 in vec2 position;
-in vec4 v_color;
 
 uniform vec2 pos;
 uniform float zoom;
 uniform float aspect;
 
-out vec4 color;
+out vec2 coord;
 
 void main() {
     vec2 p = position + pos;
     p.x /= aspect;
-    color = v_color;
+    coord = position;
     gl_Position = vec4(p*zoom, 0.0, 1.0);
 }
 `;
 
 
 export const BORDERS_FS_SOURCE = `#version 300 es
-precision mediump float;
+precision highp float;
 out vec4 fragColor;
-in vec4 color;
+in vec2 coord;
 
+uniform sampler2D uSampler;
+
+
+float getFloatValue(vec4 v) {
+    return 2.0 *
+        ((v.r * 256.0 * 256.0 * 256.0 * 255.0 +
+        v.g * 256.0 * 256.0 * 255.0 +
+        v.b * 256.0 * 255.0 +
+        v.a * 255.0) / (256.0 * 256.0 * 256.0 * 256.0)) - 1.0;
+}
+
+float getValue() {
+    float x = 60000.0 * getFloatValue(texelFetch(uSampler, ivec2(0, 0), 0));
+    return length(coord.xy - vec2(x, 0.0));
+}
 
 void main() {
-    fragColor = color;
+    float r = getValue();
+    for(int i =0;i<100000;i++){
+        r = getValue();
+    }
+    fragColor = vec4(r, 0.0, 0.0, 1.0);
 }
 `;
